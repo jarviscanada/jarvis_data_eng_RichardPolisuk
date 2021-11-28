339c958fb7b30cc3bf44e0412be3b1c647106631
Richard Polisuk <polisuk@hotmail.com>
2021-11-28 13:10:45 -0500

    Updates to usage

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -16,12 +16,13 @@ then
 fi
 
 lscpu_out=$(lscpu)
+vmstat_mb=$(vmstat --unit M)
 df_out=$(df -BM /)
 hostname=$(hostname -f)
 
-memory_free=$(echo "$(vmstat --unit M)" | awk '{print $4 }' | sed -n 3p)
-cpu_idle=$(echo "$(vmstat)" | awk 'NR==3 {print $15 }' | xargs)
-cpu_kernel=$(echo "$(vmstat -t)" | awk '{print $14}' | sed -n 3p)
+memory_free=$(echo "$vmstat_mb" | awk '{print $4 }' | sed -n 3p)
+cpu_idle=$(echo "$vmstat_mb" | awk 'NR==3 {print $15 }' | xargs)
+cpu_kernel=$(echo "$vmstat_mb" | awk '{print $14}' | sed -n 3p)
 disk_io=$(echo "$(vmstat -d)" | awk '{print 10}' | sed -n 3p)
 disk_available=$(echo "$df_out" | awk '{print $4}' | sed -n 2p | sed 's/.$//')
 
@@ -30,6 +31,6 @@ statement="INSERT INTO host_usage
           VALUES
           ((SELECT id FROM host_info WHERE hostname='$hostname'), $memory_free, $cpu_idle, $cpu_kernel, $disk_io, $disk_available)"
 
-export PGPASSWORD=psql_password
-echo psql -h "$host" -p "$port" -U "$username" -d "$db_name" -c "$statement"
+export PGPASSWORD=$psql_password
+psql -h "$psql_host" -p "$sql_port" -U "$psql_user" -d "$db_name" -c "$statement"
 exit $?
