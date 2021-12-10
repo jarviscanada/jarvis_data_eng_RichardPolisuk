06816163bdf0c4e8920a4f3e094693ec8160eb25
Richard Polisuk <polisuk@gmail.com>
2021-12-10 11:45:43 -0500

    Update README.md

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -2,34 +2,42 @@ package ca.jrvs.apps.jdbc;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JDBCExecutor {
 
   public static void main(String[] args) {
+
+    BasicConfigurator.configure(); // Use default logger config
+
+    final Logger logger = LoggerFactory.getLogger(JDBCExecutor.class);
+
     DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", "hplussport",
         "postgres", "password");
     try {
       Connection connection = dcm.getConnection();
       CustomerDAO customerDAO = new CustomerDAO(connection);
       Customer customer = new Customer();
-      customer.setFirstName("John");
-      customer.setLastName("Adams");
-      customer.setEmail("jadams.wh.gov");
-      customer.setAddress("1234 Main St");
-      customer.setCity("Arlington");
-      customer.setState("VA");
+      customer.setFirstName("Justin");
+      customer.setLastName("Trudeau");
+      customer.setEmail("pm@pm.gc.ca");
+      customer.setAddress("80 Wellington Street");
+      customer.setCity("Ottawa");
+      customer.setState("ON");
       customer.setPhone("(555) 555-9845");
-      customer.setZipCode("01234");
+      customer.setZipCode("K1A 0A2");
 
       Customer dbCustomer = customerDAO.create(customer);
-      System.out.println(dbCustomer);
+      logger.info(String.valueOf(dbCustomer));
 
       dbCustomer = customerDAO.findById(dbCustomer.getId());
-      System.out.println(dbCustomer);
+      logger.info(String.valueOf(dbCustomer));
 
-      dbCustomer.setEmail("john.adams@wh.gov");
+      dbCustomer.setEmail("justin@pm.gc.ca");
       dbCustomer = customerDAO.update(dbCustomer);
-      System.out.println(dbCustomer);
+      logger.info(String.valueOf(dbCustomer));
 
       customerDAO.delete(dbCustomer.getId());
     } catch (SQLException e) {
