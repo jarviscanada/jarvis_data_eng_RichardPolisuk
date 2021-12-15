d191b1cd0c93e040df4332966402de4b2660d36f
Richard Polisuk <polisuk@gmail.com>
2021-12-15 13:28:59 -0500

    Add ContainsDuplicate

---
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/ContainsDuplicateTest.java
@@ -0,0 +1,33 @@
+package ca.jrvs.practice.codingChallenge;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ContainsDuplicateTest {
+
+  private ContainsDuplicate containsDuplicate;
+  int[] nums1 = new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+  int[] nums2 = new int[]{2, 4, 6, 8, 10};
+
+  @Before
+  public void setUp() throws Exception {
+    System.out.println("--@Before method runs before each @Test method");
+    //instantiate a test class for each @Test method
+    containsDuplicate = new ContainsDuplicate();
+  }
+
+  @Test
+  public void containsDuplicateSorting() {
+    System.out.println("Test case: test containsDuplicateSorting method from the test class");
+    Assert.assertEquals(true, containsDuplicate.containsDuplicateSorting(nums1));
+  }
+
+  @Test
+  public void containsDuplicateSet() {
+    System.out.println("Test case: test containsDuplicateSet method from the test class");
+    Assert.assertEquals(false, containsDuplicate.containsDuplicateSet(nums2));
+  }
+}
