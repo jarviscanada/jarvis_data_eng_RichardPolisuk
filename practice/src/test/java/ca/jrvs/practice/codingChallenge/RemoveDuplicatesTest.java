ec3c31139ed5f23e4defa9450e280dd6996acad6
Richard Polisuk <polisuk@gmail.com>
2021-12-24 13:54:45 -0500

    Save tests

---
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/RemoveDuplicatesTest.java
@@ -0,0 +1,26 @@
+package ca.jrvs.practice.codingChallenge;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RemoveDuplicatesTest {
+
+  private RemoveDuplicates removeDuplicates;
+
+  @Before
+  public void setUp() throws Exception {
+    System.out.println("--@Before method runs before each @Test method");
+    //instantiate a test class for each @Test method
+    removeDuplicates = new RemoveDuplicates();
+  }
+
+  @Test
+  public void removeDuplicates() {
+    int[] nums = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 4, 4};
+    int expected = 5;
+    Assert.assertEquals(expected, removeDuplicates.removeDuplicates(nums));
+  }
+}
