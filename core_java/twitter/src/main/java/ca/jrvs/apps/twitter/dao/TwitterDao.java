d37f1a44c3d9d934e41832a8c610f9d35060bded
Richard Polisuk <polisuk@gmail.com>
2021-12-27 15:44:08 -0500

    Save

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/TwitterDao.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/TwitterDao.java
@@ -2,15 +2,22 @@ package ca.jrvs.apps.twitter.dao;
 
 import ca.jrvs.apps.twitter.dao.helper.HttpHelper;
 import ca.jrvs.apps.twitter.model.Tweet;
+import ca.jrvs.apps.twitter.util.JsonUtil;
+import com.google.gdata.util.common.base.PercentEscaper;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class TwitterDao implements CrdDao<Tweet, String> {
 
   // URI constants
   private static final String API_BASE_URI = "https://api.twitter.com";
-  private static final String POST_PATH = "";
-  private static final String SHOW_PATH = "";
-  private static final String DELETE_PATH = "";
+  private static final String POST_PATH = "/2/tweets";
+  private static final String SHOW_PATH = "/2/users/";
+  private static final String DELETE_PATH = "/2/tweets/";
 
   // URI symbols
   private static final String QUERY_SYM = "?";
@@ -35,7 +42,10 @@ public class TwitterDao implements CrdDao<Tweet, String> {
    */
   @Override
   public Tweet create(Tweet entity) {
-    return null;
+
+    URI uri = getUri(entity);
+    HttpResponse response = httpHelper.httpPost(uri);
+    return parseResponseBody(response, HTTP_OK);
   }
 
   /**
@@ -46,7 +56,10 @@ public class TwitterDao implements CrdDao<Tweet, String> {
    */
   @Override
   public Tweet findById(String s) {
-    return null;
+    String uriStr = API_BASE_URI + SHOW_PATH + QUERY_SYM + "id" + EQUAL + s;
+    URI uri = getUri(uriStr);
+    HttpResponse response = httpHelper.httpGet(uri);
+    return parseResponseBody(response, HTTP_OK);
   }
 
   /**
@@ -57,6 +70,68 @@ public class TwitterDao implements CrdDao<Tweet, String> {
    */
   @Override
   public Tweet deleteById(String s) {
-    return null;
+    String uriStr = API_BASE_URI + DELETE_PATH + "/" + s + ".json";
+    URI uri = getUri(uriStr);
+    HttpResponse response = httpHelper.httpPost(uri);
+    return parseResponseBody(response, HTTP_OK);
+  }
+
+  public Tweet parseResponseBody(HttpResponse response, Integer expectedStatusCode) {
+    Tweet tweet = null;
+
+    //check response status
+    int status = response.getStatusLine().getStatusCode();
+    if (status != expectedStatusCode) {
+      try {
+        System.out.println(EntityUtils.toString(response.getEntity()));
+      } catch (IOException e) {
+        System.out.println("Response has no entity");
+      }
+      throw new RuntimeException("Unexpected HTTP status:" + status);
+    }
+
+    if (response.getEntity() == null) {
+      throw new RuntimeException("Empty response body");
+    }
+
+    //Convert Response Entity to Str
+    String jsonStr;
+    try {
+      jsonStr = EntityUtils.toString(response.getEntity());
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to convert entity to String", e);
+    }
+
+    //Convert JSON string to Tweet object
+    try {
+      tweet = JsonUtil.toObjectFromJson(jsonStr, Tweet.class);
+    } catch (IOException e) {
+      throw new RuntimeException("Unable to convert JSON str to Object", e);
+    }
+    return tweet;
+  }
+
+  private URI getUri(String s) {
+    URI uri;
+    try {
+      uri = new URI(s);
+    } catch (URISyntaxException e) {
+      throw new IllegalArgumentException("Invalid uri string", e);
+    }
+    return uri;
+  }
+
+  private URI getUri(Tweet tweet) {
+    String textStatus = tweet.getText();
+    Double longitude = tweet.getCoordinates().getCoordinates().get(0);
+    Double latitude = tweet.getCoordinates().getCoordinates().get(1);
+
+    PercentEscaper percentEscaper = new PercentEscaper("", false);
+    String uriStr =
+        API_BASE_URI + POST_PATH + QUERY_SYM + "status" + EQUAL + percentEscaper.escape(textStatus)
+            +
+            AMPERSAND + "long" + EQUAL + longitude + AMPERSAND + "lat" + EQUAL + latitude;
+
+    return getUri(uriStr);
   }
 }
